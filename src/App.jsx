8ad6188933ef42f8edb0906ac3f552610a45ceeb
Lyrecoph <lyrecoph30@gmail.com>
2024-07-17 17:21:08 +0100

    mis en place du composant pour calculer les investissements

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
 import Header from "./components/Header.jsx"
+import Results from "./components/Results.jsx"
 import UserInput from "./components/UserInput.jsx"
 
 function App() {
+
+  /** Lifting State Up: Léve le state se trouvant dans le composant 
+      UserInput et le transmettre au composant App vu que nous devons passer
+      l'entrée de l'utilisateur  au composant Results**/
+  // gérer et stocker les valeurs fournies par l'utilisateur
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10  
+  });
+
+  // Fonction qui doit être déclenché à chaque fois que nous modifions la valeur
+  // des champs de saisies afin de pouvoir mettre jour l'etat 
+  function handleChange(inputIdentifier, newValue){
+      // mettre à jour l'état en fonction de la valeur de l'état précedent
+      setUserInput(prevUserInput => {
+          return {
+              ...prevUserInput,
+              // transforme la valeur qui est une chaîne de caractère en valeur numérique
+              [inputIdentifier]: +newValue  
+          }
+      })
+  }
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChangeInput={handleChange} />
+      <Results input={userInput} />
     </>
   )
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,26 +1,7 @@
-import { useState } from "react";
 
 // recupérer les différents éléments entrée de l'utilisateur
-export default function UserInput() {
-    // gérer et stocker les valeurs fournies par l'utilisateur
-    const [userInput, setUserInput] = useState({
-        initialInvestment: 10000,
-        annualInvestment: 1200,
-        expectedReturn: 6,
-        duration: 10
-    });
-
-    // Fonction qui doit être déclenché à chaque fois que nous modifions la valeur
-    // des champs de saisies afin de pouvoir mettre jour l'etat 
-    function handleChange(inputIdentifier, newValue){
-        // mettre à jour l'état en fonction de la valeur de l'état précedent
-        setUserInput(prevUserInput => {
-            return {
-                ...prevUserInput,
-                [inputIdentifier]: newValue
-            }
-        })
-    }
+export default function UserInput({onChangeInput, userInput}) {
+    
     return (
         <section id="user-input">
             <div className="input-group">
@@ -31,7 +12,7 @@ export default function UserInput() {
                         required 
                         value={userInput.initialInvestment}
                         onChange={(event) => 
-                            handleChange('initialInvestment', event.target.value)
+                            onChangeInput('initialInvestment', event.target.value)
                         }
                     />
                 </p>
@@ -42,7 +23,7 @@ export default function UserInput() {
                         required
                         value={userInput.annualInvestment}
                         onChange={(event) => 
-                            handleChange('annualInvestment', event.target.value)
+                            onChangeInput('annualInvestment', event.target.value)
                         }
                     />
                 </p>
@@ -56,7 +37,7 @@ export default function UserInput() {
                         required 
                         value={userInput.expectedReturn}
                         onChange={(event) => 
-                            handleChange('expectedReturn', event.target.value)
+                            onChangeInput('expectedReturn', event.target.value)
                         }
                     />
                 </p>
@@ -68,7 +49,7 @@ export default function UserInput() {
                         required 
                         value={userInput.duration}
                         onChange={(event) => 
-                            handleChange('duration', event.target.value)
+                            onChangeInput('duration', event.target.value)
                         }
                     />
                 </p>
